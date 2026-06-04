d03d3ab16dbac1256e9a154f4f4a1303a318a7e0
aarshpatel1 <aarsh18patel@gmail.com>
2026-06-04 22:37:40 +0530

    create an array of object for features list

--- src/components/ImgDescription.jsx
+++ src/components/ImgDescription.jsx
@@ -5,6 +5,21 @@ function ImgDescription() {
 		"https://picsum.photos/536/354",
 	);
 
+	const [features, setFeatures] = useState([
+		{
+			name: "Apple",
+			url: "https://cdn.pixabay.com/photo/2016/09/29/08/33/apple-1702316_640.jpg",
+		},
+		{
+			name: "Banana",
+			url: "https://media.istockphoto.com/id/619046500/photo/bananas.jpg?s=612x612&w=0&k=20&c=p5-v1iKwhOhw5cFjfx83qgaZcOBSVpUuicZi4VIGF2Y=",
+		},
+		{
+			name: "Pineapple",
+			url: "https://cdn.vectorstock.com/i/1000v/66/85/pineapple-fruit-cartoon-colored-clipart-vector-46536685.jpg",
+		},
+	]);
+
 	const updateImage = (img) => {
 		setCurrentImage(img);
 	};
@@ -15,22 +30,16 @@ function ImgDescription() {
 				<h1 id="feature-heading">Features</h1>
 				<div className="features">
 					<ul className="features-list">
-						<li
-							onMouseOver={() => updateImage("https://cdn.pixabay.com/photo/2016/09/29/08/33/apple-1702316_640.jpg")}
-						>
-							Apple
-						</li>
-						<li
-							onMouseOver={() => updateImage("https://media.istockphoto.com/id/619046500/photo/bananas.jpg?s=612x612&w=0&k=20&c=p5-v1iKwhOhw5cFjfx83qgaZcOBSVpUuicZi4VIGF2Y=")}
-						>
-							Banana
-						</li>
-						<li
-							onMouseOver={() => updateImage("https://cdn.vectorstock.com/i/1000v/66/85/pineapple-fruit-cartoon-colored-clipart-vector-46536685.jpg")}
-						>
-							Pineapple
-						</li>
-					
+						{features.map((f, index) => (
+							<li
+								key={index}
+								onMouseOver={() => updateImage(f.url)}
+								onFocus={() => updateImage(f.url)}
+								tabIndex={0}
+							>
+								{f.name}
+							</li>
+						))}
 					</ul>
 					<div className="feature-image">
 						<img src={currentImage} alt="image" className="img" />
